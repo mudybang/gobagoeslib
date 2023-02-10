e3b752043c25d846f58869f71aa484d543d9c58e
Bagoes x360 <bagoes.septian@gmail.com>
2023-02-10 09:38:52 +0700

    a

--- one.go
+++ one.go
@@ -5,7 +5,7 @@ import (
 	"strings"
 )
 
-func printMessage(message string, arr []string) {
+func PrintMessage(message string, arr []string) {
 	var nameString = strings.Join(arr, " ")
 	fmt.Println(message, nameString)
 }
